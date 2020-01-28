8d806a282115a3eced664b5bd40c7233f72703a5
rstatz <robinstatz@gmail.com>
2020-01-28 06:48:10 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toName: '',
+      toEmail: '',
+      subject: '',
+      body: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  submitForm = async () => {
+    console.log(this.state, 'this state');
+    try {
+      const emailResponse = await fetch(`https://statz-server.herokuapp.com/api/email`, {
+          method: 'POST',
+          credentials: 'same-origin',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.state)
+        });
+        const email = await emailResponse.json();
+      console.log(email)
+    } catch(e) {
+      console.log(e, "e")
+    }
+
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          To Name:
+          <input
+            name="toName"
+            value={this.state.toName}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          To Email:
+          <input
+            name="toEmail"
+            value={this.state.toEmail}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label>
+          Email Subject:
+          <input
+            name="subject"
+            value={this.state.subject}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label>
+          Email Body:
+          <input
+            name="body"
+            value={this.state.body}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <button onClick={this.submitForm}>SUBMIT</button>
+      </form>
+    );
+  }
+}
+
+export default App;
