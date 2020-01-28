8bceeb2dc82585a8f4b20c1ed3063f6877fe9c75
rstatz <robinstatz@gmail.com>
2020-01-28 06:54:08 -0700

    second commit

---
+++ build/precache-manifest.75f31e71a91746206db3cb1a90e560e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3b1f845b2f0f76052c9b9249ba19af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1abd4a0290ef929f1b6",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "15ef05c6816b8ab196f7",
+    "url": "/static/js/2.7c889569.chunk.js"
+  },
+  {
+    "revision": "d1abd4a0290ef929f1b6",
+    "url": "/static/js/main.e8b903eb.chunk.js"
+  },
+  {
+    "revision": "5d037abd8cde956186e6",
+    "url": "/static/js/runtime-main.04550c7f.js"
+  }
+]);
